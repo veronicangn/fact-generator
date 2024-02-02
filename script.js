4083893359d825a8b25f227cd3b1f62019680719
veronicangn <111832170+veronicangn@users.noreply.github.com>
2024-02-01 21:41:38 -0800

    update styles

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// retrieves fact and generate-btn id from HTML
 let fact = document.getElementById("fact");
 let btn = document.getElementById("generate-btn");
 
@@ -5,15 +6,15 @@ let options = {
     method: "GET", headers: {"x-api-key": apiKey}
 };
 
-let url = "https://api.api-ninjas.com/v1/facts?limit=1";
+let url = "https://api.api-ninjas.com/v1/facts?limit=1"; // random facts API URL
 
 let generateQuote = () => {
-    fetch(url, options)
-    .then((response) => response.json())
+    fetch(url, options)  // fetches the URL and API key
+    .then((response) => response.json())  // converts response to JSON
     .then((data) => {
-        fact.innerText = data[0].fact;
+        fact.innerText = data[0].fact;  // updates fact text with the fact
     })
 }
 
 btn.addEventListener("click", generateQuote);
-window.addEventListener("load", generateQuote);
+window.addEventListener("load", generateQuote);  // generates quote upon loading of screen
